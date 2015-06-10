4d3f691d2e77745e3e288aa0edb9106123ee819b
tyscorp <tyscorp@gmail.com>
2015-06-10 12:43:24 -0500

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+http.createServer(app).listen(3000, function () {
+    console.log('Express server listening on port', 3000);
+});
